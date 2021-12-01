f0582c8b1c74a60b13530eb8b556178c5d32e0c3
Pranoti Dhabu <pranoti.dhabu@gmail.com>
2021-12-01 15:32:51 -0500

    Committing additional test cases

--- src/test/java/edu/neu/coe/info6205/sort/counting/MSDRadixSortTest.java
+++ src/test/java/edu/neu/coe/info6205/sort/counting/MSDRadixSortTest.java
@@ -66,4 +66,80 @@ public class MSDRadixSortTest {
         System.out.println("After: " + Arrays.toString(input_devnagri));
         assertArrayEquals(expected_devnagri, input_devnagri);
     }
+
+    @Test
+    public void sort1() throws IOException {
+        String[] words = getWords("/shuffledChinese.txt", MSDRadixSortTest::lineAsList);
+        MSDRadixSort.cmp = Collator.getInstance(Locale.CHINA);
+        MSDRadixSort.sort(words);
+        assertEquals("阿安", words[0]);
+    }
+
+    @Test
+    public void testGetWords1() {
+        String[] words = getWords("/shuffledChinese.txt", MSDRadixSortTest::lineAsList);
+        assertEquals(999998, words.length);
+    }
+
+    /**
+     * Create a string representing an integer, with commas to separate thousands.
+     *
+     * @param x the integer.
+     * @return a String representing the number with commas.
+     */
+    public static String formatWhole(final int x) {
+        return String.format("%,d", x);
+    }
+
+    /**
+     * Method to open a resource relative to this class and from the corresponding File, get an array of Strings.
+     *
+     * @param resource           the URL of the resource containing the Strings required.
+     * @param stringListFunction a function which takes a String and splits into a List of Strings.
+     * @return an array of Strings.
+     */
+    static String[] getWords(final String resource, final Function<String, List<String>> stringListFunction) {
+        Class<?> clazz = MSDRadixSortTest.class;
+        try {
+            final File file = new File(Objects.requireNonNull(clazz.getResource(resource)).toURI());
+            final String[] result = getWordArray(file, stringListFunction, 2);
+            System.out.println("getWords: testing with " + formatWhole(result.length) + " unique words: from " + file);
+            return result;
+        } catch (final URISyntaxException | NullPointerException e) {
+            System.out.println("Cannot find resource: " + resource + "  relative to class: " + clazz);
+            return new String[0];
+        }
+    }
+
+    private static List<String> getWordList(final FileReader fr, final Function<String, List<String>> stringListFunction, final int minLength) {
+        final List<String> words = new ArrayList<>();
+        for (final Object line : new BufferedReader(fr).lines().toArray())
+            words.addAll(stringListFunction.apply((String) line));
+        return words.stream().distinct().filter(s -> s.length() >= minLength).collect(Collectors.toList());
+    }
+
+
+    /**
+     * Method to read given file and return a String[] of its content.
+     *
+     * @param file               the file to read.
+     * @param stringListFunction a function which takes a String and splits into a List of Strings.
+     * @param minLength          the minimum acceptable length for a word.
+     * @return an array of Strings.
+     */
+    static String[] getWordArray(final File file, final Function<String, List<String>> stringListFunction, final int minLength) {
+        try (final FileReader fr = new FileReader(file)) {
+            return getWordList(fr, stringListFunction, minLength).toArray(new String[0]);
+        } catch (final IOException e) {
+            System.out.println("Cannot open file: " + file);
+            return new String[0];
+        }
+    }
+
+    static List<String> lineAsList(final String line) {
+        final List<String> words = new ArrayList<>();
+        words.add(line);
+        return words;
+    }
+
 }
