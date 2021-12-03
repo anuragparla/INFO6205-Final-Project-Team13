3602da1f411d8dc752768b324e3912ea860bf629
Pranoti Dhabu <90432249+pranotidhabu@users.noreply.github.com>
2021-12-03 13:08:12 -0500

    Revert "committing benchmarking for dual_pivot quick sort"

--- src/main/java/edu/neu/coe/info6205/sort/HelperFactory.java
+++ src/main/java/edu/neu/coe/info6205/sort/HelperFactory.java
@@ -17,8 +17,7 @@ public class HelperFactory {
      * @return a Helper<X></X>
      */
     public static <X extends Comparable<X>> Helper<X> create(String description, int nElements, Config config) {
-        //return create(description, nElements, config.isInstrumented(), config);
-        return new BaseHelper<>(description, nElements, config);
+        return create(description, nElements, config.isInstrumented(), config);
     }
 
     /**
